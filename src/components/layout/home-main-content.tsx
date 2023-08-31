fa9df6960e1b14030ff60b5f80259a86f9c300d9
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-08-31 18:30:12 +0400

    day 7

--- src/components/layout/home-main-content.tsx
+++ src/components/layout/home-main-content.tsx
@@ -1,9 +1,18 @@
 import Frame from "../../assets/home/desktop/image-hero-phone.png";
+import BgImg from "../../assets/home/desktop/bg-pattern-hero-home.svg";
 import MobFrame from "../../assets/home/desktop/mob-image-hero-phone.png";
 import { ButtonDark } from ".";
 export const HomeMainContent = () => {
   return (
-    <div className="overflow-hidden md:rounded-[15px] bg-clrPinkDark text-white max-w-[1111px] mx-auto items-center flex flex-col lg:flex-row md:mx-10 lg:mx-auto">
+    <div
+      style={{
+        backgroundImage: `url(${BgImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundPositionX: "100%",
+        backgroundPositionY: Screen ? "40%" : "0%",
+      }}
+      className="overflow-hidden md:rounded-[15px] bg-clrPinkDark text-white max-w-[1111px] mx-auto items-center flex flex-col lg:flex-row md:mx-10 lg:mx-auto"
+    >
       <div className="text-center space-y-6 lg:text-start lg:space-y-10 lg:pl-[95px]">
         <h3 className="text-5xl  font-fo font-medium max-w-[573px] mx-auto mt-20 md:mt-[60px] lg:mt-0">
           Award-winning custom designs and digital branding solutions
