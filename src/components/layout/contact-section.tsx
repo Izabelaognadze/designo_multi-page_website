aa4a3c6e4e6d507ba74ce51e997cf7b85ebf85b2
Izabela Ognadze <75525287+Izabelaognadze@users.noreply.github.com>
2023-09-01 00:06:28 +0400

    day 7

--- src/components/layout/contact-section.tsx
+++ src/components/layout/contact-section.tsx
@@ -10,7 +10,7 @@ export const ContactSection = () => {
         backgroundRepeat: "max-width: 800px" ? "repeat" : "no-repeat",
       }}
     >
-      <div className="text-center md:text-start lg:w-[50%]">
+      <div className="text-center md:text-start lg:w-[50%] flex lg:justify-center flex-col">
         <h1 className="text-5xl pt-[72px] mb-6 lg:pt-0">Contact Us</h1>
         <p className="md:max-w-[573px] lg:max-w-[445px]">
           Ready to take it to the next level? Let&apos;s talk about your project
@@ -19,11 +19,32 @@ export const ContactSection = () => {
           users, drop us a line.
         </p>
       </div>
-      <div className="items-center flex flex-col lg:w-[50%]">
-        <div className="">
-          <input type="text" placeholder="Name" className="inputClass" />
+      <div className="items-center md:items-end flex flex-col lg:w-[50%]">
+        <div className="space-y-6 mb-10 flex flex-col lg:pt-[55px] w-full">
+          <input
+            type="text"
+            placeholder="Name"
+            className="bg-inherit placeholder:text-white border-b-[1px] border-b-white border-solid placeholder:opacity-60 border-opacity-60"
+            size={1}
+          />
+          <input
+            type="email"
+            placeholder="Email Address"
+            className="bg-inherit placeholder:text-white border-b-[1px] border-b-white border-solid placeholder:opacity-60 border-opacity-60"
+          />
+          <input
+            type="tel"
+            placeholder="Phone"
+            className="bg-inherit placeholder:text-white border-b-[1px] border-b-white border-solid placeholder:opacity-60 border-opacity-60"
+          />
+          <textarea
+            placeholder="Your Message"
+            className="bg-inherit placeholder:text-white border-b-[1px] border-b-white border-solid placeholder:opacity-60 border-opacity-60 h-[102px] placeholder:text-"
+          ></textarea>
         </div>
-        <ButtonDark label="submit" />
+        <div className="pb-[72px]">
+          <ButtonDark label="submit" />
+        </div>
       </div>
     </div>
   );
